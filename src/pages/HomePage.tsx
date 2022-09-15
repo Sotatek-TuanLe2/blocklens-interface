fa035e0c01fb2cc29154d85e24f107a5ed43e1a1
sotatek-trungbui <trung.bui@sotatek.com>
2022-09-15 13:49:00 +0700

    feat: add image sprite

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import React from 'react';
-import BasePage from "../layouts/BasePage";
+import BasePage from '../layouts/BasePage';
+import AppIcon, { LIST_ICONS } from 'src/components/AppIcon';
 
 const HomePage: FC = () => {
   return (
     <BasePage>
       Home Page
+      <AppIcon icon={LIST_ICONS.ETHEREUM_BLUE} />
     </BasePage>
   );
 };
