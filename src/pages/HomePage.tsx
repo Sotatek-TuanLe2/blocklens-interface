5f3bcba6ece310545c64f75573dfeafacfb6389e
sotatek-trungbui <trung.bui@sotatek.com>
2022-09-15 15:41:55 +0700

    fix: delete AppIcon file

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
 import { FC } from 'react';
 import React from 'react';
 import BasePage from '../layouts/BasePage';
-import AppIcon, { LIST_ICONS } from 'src/components/AppIcon';
 
 const HomePage: FC = () => {
-  return (
-    <BasePage>
-      Home Page
-      <AppIcon icon={LIST_ICONS.GRAY_DOWN_ARROW} />
-    </BasePage>
-  );
+  return <BasePage>Home Page</BasePage>;
 };
 
 export default HomePage;
