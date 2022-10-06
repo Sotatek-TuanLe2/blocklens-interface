c3581ad359048ca7d03234a9a0c5c5ddeb468ef4
Truong Mac <dangtruong1503@gmail.com>
2022-10-06 17:15:53 +0700

    feature: handle upload abi

--- src/components/AppUploadABI.tsx
+++ src/components/AppUploadABI.tsx
@@ -1,16 +1,161 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Checkbox } from '@chakra-ui/react';
 import { toastError } from 'src/utils/utils-notify';
-import { AppInput } from './index';
-import React, { useState, FC } from 'react';
+import React, { useState, FC, useMemo, ChangeEvent } from 'react';
+import {
+  AppPagination,
+  AppCard,
+  AppInput,
+  AppSelect,
+} from 'src/components';
 const Validator = require('jsonschema').Validator;
 const validateJson = new Validator();
 
 interface IAppUploadABI {
-  onChange: (value: any) => void;
+  onChange?: (value: any) => void;
 }
+
+interface IListSelect {
+  data: any;
+  title: string;
+  valueSearch: string;
+  valueSort: string;
+  dataSelected: any;
+  onSelectData: (value: any) => void;
+}
+
+const options = [
+  {
+    label: 'A - Z',
+    value: 'az',
+  },
+  {
+    label: 'Z - A',
+    value: 'za',
+  },
+];
+
+const itemsPerPage = 10;
+
+const ListSelect: FC<IListSelect> = ({
+  title,
+  data,
+  onSelectData,
+  dataSelected,
+  valueSearch,
+  valueSort,
+}) => {
+  const [page, setPage] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [itemSelected, setItemSelected] = useState<any>([]);
+
+  const onChangeSelect = (e: ChangeEvent<HTMLInputElement>, name: string) => {
+    let newItemsSelected = [];
+
+    if (!e.target.checked) {
+      newItemsSelected = [
+        ...itemSelected.filter((item: string) => item !== name),
+      ];
+      onSelectData([...dataSelected.filter((item: any) => item.name !== name)]);
+    } else {
+      newItemsSelected = [...itemSelected, name];
+      onSelectData([
+        ...dataSelected,
+        data.filter((item: any) => item.name === name)[0],
+      ]);
+    }
+    setItemSelected(newItemsSelected);
+  };
+
+  const onChangePagination = (event: { selected: number }) => {
+    setPage(event.selected);
+  };
+
+  const dataShow = useMemo(() => {
+    let dataFiltered = data;
+
+    if (!!valueSearch) {
+      dataFiltered = data.filter((item: any) =>
+        item.name.includes(valueSearch),
+      );
+    }
+
+    if (valueSort === 'az') {
+      dataFiltered = data.sort((a: any, b: any) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+
+    if (valueSort === 'za') {
+      dataFiltered = data.sort((a: any, b: any) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return 1;
+        }
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+
+    setTotalPages(Math.ceil(dataFiltered.length / itemsPerPage));
+
+    return dataFiltered.slice(
+      page * itemsPerPage,
+      page * itemsPerPage + itemsPerPage,
+    );
+  }, [data, page, valueSearch, valueSort]);
+
+  return (
+    <AppCard mt={5} pt={0}>
+      <Box fontSize={'18px'} mb={5}>
+        {title}
+      </Box>
+      {!!dataShow.length ? (
+        dataShow?.map((item: any, index: number) => {
+          return (
+            <Box key={index} my={2}>
+              <Checkbox
+                value={item.name}
+                isChecked={itemSelected.includes(item.name)}
+                onChange={(e) => onChangeSelect(e, item.name)}
+              >
+                {item.name}
+              </Checkbox>
+            </Box>
+          );
+        })
+      ) : (
+        <Flex justifyContent={'center'}>
+          <Box> No data...</Box>
+        </Flex>
+      )}
+
+      {totalPages > 0 && (
+        <Flex justifyContent={'flex-end'}>
+          <AppPagination
+            pageCount={totalPages}
+            forcePage={page}
+            onPageChange={onChangePagination}
+          />
+        </Flex>
+      )}
+    </AppCard>
+  );
+};
 
 const AppUploadABI: FC<IAppUploadABI> = ({ onChange }) => {
   const [fileSelected, setFileSelected] = useState<any>({});
+  const [ABIData, setABIData] = useState<any>([]);
+  const [dataSelected, setDataSelected] = useState<any>([]);
+  const [valueSearch, setValueSearch] = useState<string>('');
+  const [valueSort, setValueSort] = useState<string>('az');
+
   const handleFileSelect = (evt: any) => {
     const file = evt.target.files[0];
     if (file.type !== 'application/json') {
@@ -79,10 +224,27 @@ const AppUploadABI: FC<IAppUploadABI> = ({ onChange }) => {
         return;
       }
       setFileSelected(evt.target.files[0]);
-      onChange(JSON.parse(data));
+      setABIData(JSON.parse(data).abi);
     };
     reader.readAsText(file);
   };
+
+  const events = useMemo(
+    () =>
+      ABIData.filter((item: any) => {
+        return item.type === 'event';
+      }),
+    [ABIData],
+  );
+
+  const functions = useMemo(
+    () =>
+      ABIData.filter((item: any) => {
+        return item.type === 'function';
+      }),
+    [ABIData],
+  );
+
   return (
     <Box>
       <Flex alignItems={'center'}>
@@ -106,7 +268,50 @@ const AppUploadABI: FC<IAppUploadABI> = ({ onChange }) => {
           <AppInput type="file" onChange={handleFileSelect} display="none" />
         </label>
       </Flex>
-      <Box mt={2}>{fileSelected?.name}</Box>
+
+      {ABIData && !!ABIData.length && (
+        <>
+          <Flex justifyContent={'space-between'}>
+            <Box mt={2}>{fileSelected?.name}</Box>
+            <Flex>
+              <Box width={'200px'}>
+                <AppSelect
+                  onChange={(e: any) => {
+                    setValueSort(e.value);
+                  }}
+                  options={options}
+                  defaultValue={options[0]}
+                />
+              </Box>
+              <AppInput
+                ml={10}
+                type="text"
+                placeholder={'Search'}
+                value={valueSearch}
+                onChange={(e) => setValueSearch(e.target.value.trim())}
+              />
+            </Flex>
+          </Flex>
+
+          <ListSelect
+            title={'Functions'}
+            data={functions}
+            dataSelected={dataSelected}
+            onSelectData={setDataSelected}
+            valueSearch={valueSearch}
+            valueSort={valueSort}
+          />
+
+          <ListSelect
+            title={'Events'}
+            data={events}
+            dataSelected={dataSelected}
+            onSelectData={setDataSelected}
+            valueSearch={valueSearch}
+            valueSort={valueSort}
+          />
+        </>
+      )}
     </Box>
   );
 };
