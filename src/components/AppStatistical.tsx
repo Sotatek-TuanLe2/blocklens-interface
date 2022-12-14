6a211c8a25902a15626684bc196d41d3460f3cb6
Truong Mac <dangtruong1503@gmail.com>
2022-12-14 15:04:50 +0700

    fix: revert data chart

--- src/components/AppStatistical.tsx
+++ src/components/AppStatistical.tsx
@@ -36,7 +36,7 @@ export const ChartStatics: FC<IChartStatics> = ({ dataChart, keyStat }) => {
         className={isMobile ? 'chartMobile' : 'chart'}
       >
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart width={500} height={300} data={dataFormat}>
+          <LineChart width={500} height={300} data={dataFormat.reverse()}>
             <Line
               type="monotone"
               dataKey={keyStat}
