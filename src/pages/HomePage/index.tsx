4e5ef87e8882e37b091b32911659d54472883c42
Sotatek-thangtrinh <thang.trinh@sotatek.com>
2022-12-28 10:00:54 +0700

    fix: get total active app

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,105 +1,99 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import 'src/styles/pages/HomePage.scss';
-import { Flex, Box } from '@chakra-ui/react';
+import {Flex, Box} from '@chakra-ui/react';
 import ListApps from './parts/ListApps';
 import PartUserGraph from './parts/PartUserGraph';
-import { BasePageContainer } from 'src/layouts';
+import {BasePageContainer} from 'src/layouts';
 import PartUserStats from './parts/PartUserStats';
-import { AppButton, AppCard } from 'src/components';
+import {AppButton, AppCard, RequestParams} from 'src/components';
 import ModalCreateApp from 'src/modals/ModalCreateApp';
 import rf from 'src/requests/RequestFactory';
-import { toastError } from 'src/utils/utils-notify';
+import { functions } from 'lodash';
+
+type AppStatsType = {
+  totalApp: number;
+  totalAppActive: number;
+  totalAppInActive: number;
+  totalRegistration: number;
+  totalRegistrationActive: number;
+}
 
 const HomePage = () => {
-  const [totalApps, setTotalApps] = useState<any>({});
-  const [open, setOpen] = useState<boolean>(false);
-  const [searchListApp, setSearchListApp] = useState<any>({});
-  const [appStat, setAppStat] = useState<any>({});
-
-  const getTotalApp = async () => {
-    try {
-      const res = await rf.getRequest('AppRequest').getListApp();
-      setTotalApps(res?.totalDocs);
-      return res;
-    } catch (error: any) {
-      toastError({ message: error?.message || 'Oops. Something went wrong!' });
-    }
-  };
-
+  const [openModalCreateApp, setOpenModalCreateApp] = useState<boolean>(false);
+  const [searchListApp, setSearchListApp] = useState<RequestParams>({});
+  const [appStat, setAppStat] = useState<AppStatsType>({} as AppStatsType);
+  
   const getAppStatOfUser = useCallback(async () => {
-    try {
-      const res = await rf.getRequest('AppRequest').getAppStatsOfUser();
-      setAppStat(res);
-    } catch (error: any) {
-      setAppStat([]);
-    }
+	try {
+	  const res = await rf.getRequest('AppRequest').getAppStatsOfUser();
+	  setAppStat(res);
+	} catch (error: any) {
+	  setAppStat({} as AppStatsType);
+	}
   }, []);
-
+  
+  const onCreateAppSuccess = async () => {
+	await getAppStatOfUser();
+	setSearchListApp(prevState => prevState)
+  }
+  
+  
   useEffect(() => {
-    getTotalApp().then();
-    getAppStatOfUser().then();
-  }, []);
-
+	(async () => await getAppStatOfUser())()
+  }, [getAppStatOfUser]);
+  
   useEffect(() => {
-    if (totalApps === 0) {
-      setOpen(true);
-    }
-  }, [totalApps]);
+	if (appStat.totalApp !== 0) return;
+	setOpenModalCreateApp(true);
+  }, [appStat.totalApp]);
 
+  
+  
   const _renderNoApp = () => {
-    return (
-      <AppCard className={'no-app'}>
-        <Flex my={14} flexDirection={'column'} alignItems={'center'}>
-          <Box className={'no-app__title'}>You don’t have any Apps</Box>
-          <Box className={'no-app__description'}>
-            Create a new App to start using Blocksniper API
-          </Box>
-          <AppButton
-            className={'no-app__btn'}
-            size={'md'}
-            onClick={() => setOpen(true)}
-          >
-            Create New App
-          </AppButton>
-        </Flex>
-      </AppCard>
-    );
+	return (
+	  <AppCard className={'no-app'}>
+		<Flex my={14} flexDirection={'column'} alignItems={'center'}>
+		  <Box className={'no-app__title'}>You don’t have any Apps</Box><Box className={'no-app__description'}>
+			Create a new App to start using Blocksniper API
+		  </Box>
+		  <AppButton
+			className={'no-app__btn'}
+			size={'md'}
+			onClick={() => setOpenModalCreateApp(true)}
+		  >
+			Create New App
+		  </AppButton>
+		</Flex>
+	  </AppCard>
+	);
   };
-
+  
   return (
-    <BasePageContainer>
-      <>
-        {totalApps > 0 ? (
-          <>
-            <PartUserStats
-              totalWebhookActive={appStat?.totalRegistrationActive}
-            />
-            <ListApps
-              totalAppActive={appStat?.totalAppActive}
-              totalApps={totalApps}
-              setOpenModalCreateApp={() => setOpen(true)}
-              searchListApp={searchListApp}
-            />
-            <PartUserGraph />
-          </>
-        ) : (
-          _renderNoApp()
-        )}
-
-        {open && (
-          <ModalCreateApp
-            open={open}
-            onClose={() => setOpen(false)}
-            reloadData={() => {
-              getTotalApp().then();
-              setSearchListApp((pre: any) => {
-                return { ...pre };
-              });
-            }}
-          />
-        )}
-      </>
-    </BasePageContainer>
+	<BasePageContainer>
+	  <>
+		{appStat.totalApp > 0 ? (
+		  <>
+			<PartUserStats
+			  totalWebhookActive={appStat?.totalRegistrationActive}
+			/>
+			<ListApps
+			  totalAppActive={appStat?.totalAppActive}
+			  totalApps={appStat.totalApp}
+			  setOpenModalCreateApp={() => setOpenModalCreateApp(true)}
+			  searchListApp={searchListApp}
+			/>
+			<PartUserGraph/>
+		  </>
+		) : (
+		  _renderNoApp()
+		)}
+		<ModalCreateApp
+		  open={openModalCreateApp}
+		  onClose={() => setOpenModalCreateApp(false)}
+		  reloadData={onCreateAppSuccess}
+		/>
+	  </>
+	</BasePageContainer>
   );
 };
 
