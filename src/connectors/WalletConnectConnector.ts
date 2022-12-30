1503c57e6a6acd1e461921f8ece6eea682a3ae5c
tuan.le2 <tuan.le2@sotatek.com>
2022-12-30 17:15:08 +0700

    fix: update signMessage in walletconnect

--- src/connectors/WalletConnectConnector.ts
+++ src/connectors/WalletConnectConnector.ts
@@ -1,4 +1,7 @@
+import { Web3Provider } from '@ethersproject/providers';
 import { WalletConnectConnector as WalletConnectProvider } from '@web3-react/walletconnect-connector';
+import config from 'src/config';
+import Storage from 'src/utils/utils-storage';
 import { WALLET_CONNECT } from '.';
 import BaseConnector from './BaseConnector';
 
@@ -22,6 +25,35 @@ class WalletConnectConnector extends BaseConnector {
     this.connector = connector;
     this.provider = provider;
     return provider;
+  }
+
+  async signMessage(): Promise<any> {
+    if (this.account && this.connector && this.provider) {
+      try {
+        const signer = new Web3Provider(this.provider).getSigner();
+        const domain = {
+          name: config.auth.domain,
+          version: '2',
+          chainId: Storage.getChainId() || config.defaultChainId,
+        };
+        const types = {
+          message: [
+            { name: 'address', type: 'address' },
+            { name: 'message', type: 'string' },
+          ],
+        };
+        const message = {
+          address: this.account,
+          message: config.auth.message,
+        };
+        const signature = await signer._signTypedData(domain, types, message);
+        return signature;
+      } catch (error) {
+        console.error(error);
+        console.error('Signing message failed!');
+        throw error;
+      }
+    }
   }
 }
 
