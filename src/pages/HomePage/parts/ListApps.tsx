8b211737fab7e301f1f27dadea530b8ebd78949a
Truong Mac <dangtruong1503@gmail.com>
2022-12-30 14:36:01 +0700

    fix: UI mobile

--- src/pages/HomePage/parts/ListApps.tsx
+++ src/pages/HomePage/parts/ListApps.tsx
@@ -48,16 +48,15 @@ const AppMobile: FC<IAppMobile> = ({ app }) => {
           justifyContent="space-between"
           alignItems="center"
           className="info"
+          onClick={() => history.push(`/apps/${app.appId}`)}
         >
-          <Box
-            className="name-mobile"
-            onClick={() => history.push(`/apps/${app.appId}`)}
-          >
-            {app.name}
-          </Box>
+          <Box className="name-mobile">{app.name}</Box>
           <Box
             className={isOpen ? 'icon-minus' : 'icon-plus'}
-            onClick={() => setIsOpen(!isOpen)}
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsOpen(!isOpen);
+            }}
           />
         </Flex>
         <Flex
