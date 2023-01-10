66118a9ac4776207d2f286da411191d1bfc4c2aa
Truong Mac <dangtruong1503@gmail.com>
2023-01-10 17:20:04 +0700

    fix: Update choose payment method

---
+++ src/modals/ModalChangePaymentMethod.tsx
@@ -0,0 +1,115 @@
+import React, { FC } from 'react';
+import BaseModal from './BaseModal';
+import { Box, Flex } from '@chakra-ui/react';
+import { AppButton } from 'src/components';
+import useUser from 'src/hooks/useUser';
+import { PAYMENT_METHOD } from 'src/pages/BillingPage';
+import FormCard from '../pages/BillingPage/parts/FormCard';
+import rf from 'src/requests/RequestFactory';
+import { toastError, toastSuccess } from 'src/utils/utils-notify';
+import { getUserProfile } from 'src/store/user';
+import { useDispatch } from 'react-redux';
+import { formatShortText } from 'src/utils/utils-helper';
+
+interface IModalChangePaymentMethod {
+  open: boolean;
+  onClose: () => void;
+  paymentMethodSelected: any;
+}
+
+const ModalChangePaymentMethod: FC<IModalChangePaymentMethod> = ({
+  open,
+  onClose,
+  paymentMethodSelected,
+}) => {
+  const { user } = useUser();
+  const dispatch = useDispatch();
+
+  const onChange = async () => {
+    try {
+      await rf
+        .getRequest('UserRequest')
+        .editInfoUser({ activePaymentMethod: paymentMethodSelected });
+      toastSuccess({ message: 'Update Successfully!' });
+      await dispatch(getUserProfile());
+      onClose();
+    } catch (error: any) {
+      toastError({ message: error.message });
+    }
+  };
+
+  const getDescription = () => {
+    if (paymentMethodSelected === PAYMENT_METHOD.CRYPTO) {
+      return 'Change payment method to crypto?';
+    }
+
+    if (!user?.getStripePayment()) {
+      return (
+        'Change payment method to card?' + '\n' + 'Please enter your card info'
+      );
+    }
+
+    return 'Change payment method to card?';
+  };
+
+  const _renderContent = () => {
+    if (paymentMethodSelected === PAYMENT_METHOD.CRYPTO) {
+      return (
+        <>
+          <Flex className="info-payment">
+            <Box className="label">Total</Box>
+            <Box className="value">${user?.getBalance()}</Box>
+          </Flex>
+          <Flex className="info-payment">
+            <Box className="label">Linked Address</Box>
+            <Box className="value">
+              {formatShortText(user?.getLinkedAddress() || '')}
+            </Box>
+          </Flex>
+        </>
+      );
+    }
+
+    if (!user?.getStripePayment()) {
+      return <FormCard isEdit onClose={onClose} onSuccess={onChange} />;
+    }
+
+    return (
+      <Flex className="info-payment">
+        <Box className="label">{user?.getStripePayment()?.card?.brand}</Box>
+        <Box className="value">-{user?.getStripePayment()?.card?.last4}</Box>
+      </Flex>
+    );
+  };
+
+  return (
+    <BaseModal
+      size="xl"
+      title={'Change Payment Method'}
+      description={getDescription()}
+      isOpen={open}
+      onClose={onClose}
+    >
+      <Box>{_renderContent()}</Box>
+
+      {(paymentMethodSelected === PAYMENT_METHOD.CRYPTO ||
+        user?.getStripePayment()) && (
+        <Flex flexWrap={'wrap'} justifyContent={'space-between'} mt={4}>
+          <AppButton
+            width={'49%'}
+            size={'lg'}
+            variant={'cancel'}
+            onClick={onClose}
+          >
+            Cancel
+          </AppButton>
+          <AppButton width={'49%'} size={'lg'} onClick={onChange}>
+            Confirm
+          </AppButton>
+        </Flex>
+      )}
+    </BaseModal>
+  );
+};
+
+export default ModalChangePaymentMethod;
