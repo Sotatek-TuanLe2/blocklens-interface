ff337b039f44acdf8836f6a5f0bacd0c94d4bad6
Truong Mac <dangtruong1503@gmail.com>
2023-01-12 10:30:25 +0700

    fix: update link terms

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -128,9 +128,11 @@ const LoginPage: FC = () => {
             </Box>
 
             <Box className="note" mt={3}>
-              This site is protected by reCAPTCHA and the {' '}
-              <AppLink to={'#'}>Privacy Policy </AppLink> and{' '}
-              <AppLink to={'#'}>Terms of Service</AppLink> apply.
+              This site is protected by reCAPTCHA and the{' '}
+              <a href="https://blocklens.io/privacy-policy" className={"link"} target="_blank">
+                Privacy Policy{' '}
+              </a>{' '}
+              and <a href="https://blocklens.io/terms" target="_blank" className={"link"}>Terms of Service</a> apply.
             </Box>
           </Box>
         </AppCard>
