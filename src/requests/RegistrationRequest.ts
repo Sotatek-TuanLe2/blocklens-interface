99814737d772207ce6dd0c176ffbaa4e3d34d67e
Truong Mac <dangtruong1503@gmail.com>
2023-02-10 10:57:06 +0700

    fix: update api for registration

--- src/requests/RegistrationRequest.ts
+++ src/requests/RegistrationRequest.ts
@@ -7,27 +7,27 @@ export default class RegistrationRequest extends BaseRequest {
   }
 
   getRegistrations(appId: string, params: any) {
-    const url = `/app-${appId}/registrations`;
+    const url = `/registrations/app-${appId}`;
     return this.get(url, params);
   }
 
   addRegistrations(appId: string, params: any) {
-    const url = `/app-${appId}/registrations`;
+    const url = `/registrations/app-${appId}`;
     return this.post(url, params);
   }
 
   updateStatus(appId: string, registrationId: string, params: any) {
-    const url = `/app-${appId}/registrations/${registrationId}`;
+    const url = `/registrations/${registrationId}`;
     return this.patch(url, params);
   }
 
   deleteRegistration(appId: string, registrationId: string) {
-    const url = `/app-${appId}/registrations/${registrationId}`;
+    const url = `/registrations/${registrationId}`;
     return this.delete(url);
   }
 
   getRegistration(appId: string, registrationId: any) {
-    const url = `/app-${appId}/registrations/${registrationId}`;
+    const url = `/registrations/${registrationId}`;
     return this.get(url);
   }
 }
