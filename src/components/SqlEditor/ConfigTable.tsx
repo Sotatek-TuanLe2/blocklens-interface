0ee36f5b5ee7607b6e76db81fbc906a33e3e1a10
dinh123 <dinhtk223@gmail.com>
2023-04-14 00:01:09 +0700

    success flow config table

--- src/components/SqlEditor/ConfigTable.tsx
+++ src/components/SqlEditor/ConfigTable.tsx
@@ -1,15 +1,15 @@
 import { Checkbox, Divider, Grid, GridItem, Text } from '@chakra-ui/react';
-import { flexRender } from '@tanstack/react-table';
-import { Table } from '@tanstack/table-core';
-import { FC, useState } from 'react';
+import { ColumnDef, Table } from '@tanstack/react-table';
+import { useState } from 'react';
 import 'src/styles/components/TableConfig.scss';
 import AppButton from '../AppButton';
 import AppInput from '../AppInput';
 import AppSelect2 from '../AppSelect2';
 
-interface IConfigTable {
-  newColumns: any;
-  updateDataNewTable: any;
+interface IConfigTable<T> {
+  newColumns: ColumnDef<T, unknown>[];
+  updateDataNewTable: (data: ColumnDef<T, unknown>) => void;
+  table: Table<T>;
 }
 interface IOption {
   value: string;
@@ -27,7 +27,14 @@ const optionAlign: IOption[] = [
   { value: 'right', label: 'Right' },
 ];
 
-const ConfigTable: FC<IConfigTable> = ({ newColumns, updateDataNewTable }) => {
+const ConfigTable = <T,>({
+  newColumns,
+  updateDataNewTable,
+  table,
+}: IConfigTable<T>) => {
+  const typeData = table
+    .getRowModel()
+    .rows.map((row) => row.getVisibleCells().map((cells) => cells.getValue()));
   return (
     <div className="main-layout">
       <header>
@@ -37,7 +44,7 @@ const ConfigTable: FC<IConfigTable> = ({ newColumns, updateDataNewTable }) => {
       </header>
       <Grid templateColumns="repeat(4, 1fr)">
         <GridItem>
-          <div className="box-table">
+          <div className="box-table first-box-table">
             <Text fontWeight="bold" marginBottom="10px">
               Table options
             </Text>
@@ -54,11 +61,13 @@ const ConfigTable: FC<IConfigTable> = ({ newColumns, updateDataNewTable }) => {
       </Grid>
       <Divider orientation="horizontal" borderColor={'gray'} />
       <Grid templateColumns="repeat(4, 1fr)" gap={'10px'}>
-        {newColumns.map((header: any) => (
-          <GridItem key={header.index}>
+        {newColumns.map((header, index) => (
+          <GridItem key={index}>
             <TableOptions
               header={header}
               updateDataNewTable={updateDataNewTable}
+              typeData={typeData}
+              index={index}
             />
           </GridItem>
         ))}
@@ -69,17 +78,25 @@ const ConfigTable: FC<IConfigTable> = ({ newColumns, updateDataNewTable }) => {
 
 export default ConfigTable;
 
-const TableOptions = ({ header, updateDataNewTable }: any) => {
+const TableOptions = ({ header, updateDataNewTable, typeData, index }: any) => {
   const [selectedItem, setSelectedItem] = useState(Object);
-
+  const typeValue = typeof typeData[0][index];
   return (
     <div className="box-table" onClick={() => setSelectedItem(header)}>
       <Text fontWeight="bold" marginBottom="10px">
-        Column {header.index}: {header.accessorKey}
+        Column {index}: {header.accessorKey}
       </Text>
       <div className="box-table-children">
         <div>Title</div>
-        <AppInput placeholder="Price" size={'sm'} className="input-table" />
+        <AppInput
+          value={header?.header}
+          onChange={(e) =>
+            updateDataNewTable({ ...selectedItem, header: e.target.value })
+          }
+          placeholder="Price"
+          size={'sm'}
+          className="input-table"
+        />
       </div>
       <div className="box-table-children">
         <div className="ok">Align</div>
@@ -96,17 +113,20 @@ const TableOptions = ({ header, updateDataNewTable }: any) => {
         <div>Format</div>
         <AppInput placeholder="0.0" size={'sm'} className="input-table" />
       </div>
-      <div className="box-table-children">
-        <div>Type</div>
+      {typeValue === 'number' && (
+        <div className="box-table-children">
+          <div>Type</div>
 
-        <AppSelect2
-          className="select-table"
-          size="small"
-          value={header?.type}
-          onChange={(e) => updateDataNewTable({ ...selectedItem, type: e })}
-          options={optionType}
-        />
-      </div>
+          <AppSelect2
+            className="select-table"
+            size="small"
+            value={header?.type}
+            onChange={(e) => updateDataNewTable({ ...selectedItem, type: e })}
+            options={optionType}
+          />
+        </div>
+      )}
+
       <div className="main-checkbox">
         <Checkbox
           size="sm"
@@ -118,7 +138,7 @@ const TableOptions = ({ header, updateDataNewTable }: any) => {
           Hide column
         </Checkbox>
       </div>
-      {header?.type === 'normal' ? (
+      {typeValue === 'number' && header?.type === 'normal' ? (
         <div>
           <div className="main-checkbox">
             <Checkbox
@@ -153,12 +173,11 @@ const TableOptions = ({ header, updateDataNewTable }: any) => {
         <div className="main-checkbox">
           <Checkbox
             size="sm"
-            value={header?.coloredPositive}
+            value={header?.coloredProgress}
             onChange={(e) =>
               updateDataNewTable({
                 ...selectedItem,
-                coloredPositive: e.target.checked,
-                coloredNegative: e.target.checked,
+                coloredProgress: e.target.checked,
               })
             }
           >
