25c2692b80b5ea104d1298478ea1a835f27685f1
tuan.le2 <tuan.le2@sotatek.com>
2023-04-25 15:35:29 +0700

    fix: fix Pie chart

--- src/components/Charts/PieChart.tsx
+++ src/components/Charts/PieChart.tsx
@@ -12,6 +12,7 @@ import CustomTooltip from './CustomTooltip';
 import CustomLegend from './CustomLegend';
 import { Flex } from '@chakra-ui/react';
 import { ChartProps } from './LineChart';
+import BigNumber from 'bignumber.js';
 
 type ChartConfigType = VisualizationOptionsType;
 type Props = ChartProps & {
@@ -59,20 +60,20 @@ const VisualizationPieChart = ({
 
     const groupedData = data.reduce(
       (acc: { [key: string]: number }, item: any) => {
-        if (typeof item[yAxisKeys?.[0]]) {
-          acc[item[xAxisKey || 0]] = [yAxisKeys?.[0]].length;
-        } else {
-          acc[item[xAxisKey || 0]] = item[yAxisKeys?.[0]];
-        }
+        acc[item[xAxisKey || 0]] = item[yAxisKeys?.[0]];
         return acc;
       },
       {},
     );
 
-    const reducedData = Object.keys(groupedData).map((name) => {
-      return { [xAxisKey || 0]: name, [yAxisKeys?.[0]]: groupedData[name] };
-    });
-
+    const reducedData = Object.keys(groupedData)
+      .filter((name) => {
+        const isNumber = !new BigNumber(groupedData[name]).isNaN();
+        return isNumber && new BigNumber(groupedData[name]).isGreaterThan(0);
+      })
+      .map((name) => {
+        return { [xAxisKey || 0]: name, [yAxisKeys?.[0]]: +groupedData[name] };
+      });
     return reducedData;
   }
 
