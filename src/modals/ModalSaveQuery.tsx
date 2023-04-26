7bb4d9589ca586698cb3bf5c8182dd5e0aca2c96
sotatek-trungbui <trung.bui@sotatek.com>
2023-04-26 17:24:58 +0700

    fix: status of submit btn in name query modal

--- src/modals/ModalSaveQuery.tsx
+++ src/modals/ModalSaveQuery.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import _ from 'lodash';
 import React, { useState } from 'react';
 import { AppButton, AppInput } from 'src/components';
 import BaseModal from './BaseModal';
@@ -11,21 +12,21 @@ export interface IModalSaveQuery {
 
 const ModalSaveQuery = ({ open, onClose, onSubmit }: IModalSaveQuery) => {
   const [nameQuerry, setNameQuerry] = useState('');
+  const [disableNameQueryBtn, setIsDisableNameQueryBtn] = useState(false);
 
   const handleChangeNameQuery = (
     event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     setNameQuerry(event.target.value);
+    !!_.trim(event.target.value)
+      ? setIsDisableNameQueryBtn(false)
+      : setIsDisableNameQueryBtn(true);
   };
   return (
     <BaseModal
       size="xl"
       isOpen={open}
       onClose={onClose}
-      onActionLeft={onClose}
-      textActionLeft="Cancel"
-      onActionRight={() => onSubmit(nameQuerry)}
-      textActionRight="Save"
       isHideCloseIcon={true}
       className="modal-save-query"
     >
@@ -37,6 +38,17 @@ const ModalSaveQuery = ({ open, onClose, onSubmit }: IModalSaveQuery) => {
           placeholder="Query name ..."
         />
         <Box pt={'20px'}>Don't worry, you can change this any time.</Box>
+        <Flex className="footer-modal">
+          <AppButton variant="outline" onClick={() => onClose()}>
+            Cancel
+          </AppButton>
+          <AppButton
+            disabled={disableNameQueryBtn}
+            onClick={() => onSubmit(nameQuerry)}
+          >
+            Save
+          </AppButton>
+        </Flex>
       </Box>
     </BaseModal>
   );
