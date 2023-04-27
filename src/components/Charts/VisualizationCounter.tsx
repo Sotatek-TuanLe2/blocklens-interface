66e975ad8fd82f715d1ea292b27d6009ee7cd62c
DinhTran <dinh.tran2@sotatek.com>
2023-04-27 11:14:40 +0700

    fix logic counter

--- src/components/Charts/VisualizationCounter.tsx
+++ src/components/Charts/VisualizationCounter.tsx
@@ -1,6 +1,6 @@
+import BigNumber from 'bignumber.js';
 import React, { useEffect, useState } from 'react';
 import 'src/styles/components/CounterConfigurations.scss';
-import { VISUALIZATION_COLORS } from 'src/utils/common';
 import { VisualizationType } from 'src/utils/query.type';
 import { roundAndPadZeros } from 'src/utils/utils-format';
 
@@ -26,9 +26,9 @@ const VisualizationCounter = ({ data, visualization }: Props) => {
   const defaultSize =
     NUMBER_SIZE.start -
     (dataCounter().length +
-      Number(dataOptions?.stringDecimal) +
-      dataOptions?.stringPrefix?.toString().length +
-      dataOptions?.stringSuffix?.toString().length) /
+      Number(dataOptions?.stringDecimal || 0) +
+      (dataOptions?.stringPrefix?.toString().length || 0) +
+      (dataOptions?.stringSuffix?.toString().length || 0)) /
       NUMBER_SIZE.percent;
   useEffect(() => {
     setSize(defaultSize);
@@ -39,8 +39,19 @@ const VisualizationCounter = ({ data, visualization }: Props) => {
     dataOptions.rowNumber,
   ]);
 
-  const isNumber = checkTypeValue === 'number';
-
+  const isNumber = !new BigNumber(dataCounter()).isNaN();
+  const checkColor = (value: string | number) => {
+    switch (true) {
+      case new BigNumber(value).isGreaterThan(0) &&
+        dataOptions.coloredPositiveValues:
+        return '#006400';
+      case new BigNumber(value).isLessThan(0) &&
+        dataOptions.coloredNegativeValues:
+        return '#d93025';
+      default:
+        return undefined;
+    }
+  };
   return (
     <div className="main-counter">
       <div className="counter-result">
@@ -48,22 +59,20 @@ const VisualizationCounter = ({ data, visualization }: Props) => {
         <div className="text-result">
           <div
             style={{
-              color: dataOptions.coloredPositiveValues
-                ? VISUALIZATION_COLORS.POSITIVE
-                : '',
-              fontSize: `${size}px`,
+              color: (isNumber && checkColor(dataCounter())) || '',
+              fontSize: ` ${size}px`,
             }}
           >
-            {isNumber && dataOptions.stringPrefix}
+            {checkTypeValue === 'number' && dataOptions.stringPrefix}
             <span>
-              {isNumber
+              {checkTypeValue === 'number'
                 ? roundAndPadZeros(
                     dataCounter(),
                     Number(dataOptions.stringDecimal || 0),
                   )
                 : dataCounter()}
             </span>
-            {isNumber && dataOptions.stringSuffix}
+            {checkTypeValue === 'number' && dataOptions.stringSuffix}
           </div>
           <div className="counter-sub-label">
             {dataOptions.counterLabel ? dataOptions.counterLabel : 'Counter'}
