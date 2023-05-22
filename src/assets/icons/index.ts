5ded38f210294d8f2d4024536e65629af5372b94
tuan.le2 <tuan.le2@sotatek.com>
2023-05-22 08:41:20 +0700

    fix: remove unused images

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -65,14 +65,6 @@ import { ReactComponent as IconHeartFavorite } from './IconHeartFavorite.svg';
 import { ReactComponent as IconOptions } from './IconOptions.svg';
 import { ReactComponent as IconCopy } from './IconCopy.svg';
 import { ReactComponent as IconShare } from './IconShare.svg';
-import { ReactComponent as ExploreV2Icon } from './ExploreV2Icon.svg';
-import { ReactComponent as ExploreActiveV2Icon } from './ExploreActiveV2Icon.svg';
-import { ReactComponent as FolderV2Icon } from './FolderV2Icon.svg';
-import { ReactComponent as FolderActiveV2Icon } from './FolderActiveV2Icon.svg';
-import { ReactComponent as CloseBtnV2Icon } from './CloseBtnV2Icon.svg';
-import { ReactComponent as PlusV2Icon } from './PlusV2Icon.svg';
-import { ReactComponent as LogoDashboardV2Icon } from './LogoDashboardV2Icon.svg';
-import { ReactComponent as LogoQueryV2Icon } from './LogoQueryV2Icon.svg';
 import { ReactComponent as FireIconInactive } from './FireIconInactive.svg';
 import { ReactComponent as IconFork } from './IconFork.svg';
 
@@ -144,14 +136,6 @@ export {
   IconOptions,
   IconCopy,
   IconShare,
-  ExploreV2Icon,
-  ExploreActiveV2Icon,
-  FolderV2Icon,
-  FolderActiveV2Icon,
-  CloseBtnV2Icon,
-  PlusV2Icon,
-  LogoDashboardV2Icon,
-  LogoQueryV2Icon,
   FireIconInactive,
   IconFork,
 };
