835712fc3cf1e625256e91419ddb00dd63cd957f
Truong Mac <truong.mac@sotatek.com>
2023-06-16 10:38:06 +0700

    fix: update icon

--- src/pages/WorkspacePage/parts/Dashboard.tsx
+++ src/pages/WorkspacePage/parts/Dashboard.tsx
@@ -317,11 +317,9 @@ const DashboardPart: React.FC = () => {
         {_renderDashboard()}
         {editMode && !isEmptyDashboard && (
           <Menu>
-            <Box className="add-button">
-              <MenuButton className="app-query-menu">
-                <img src={PlusIcon} alt="icon-plus" />
-              </MenuButton>
-            </Box>
+            <MenuButton className="app-query-menu add-button">
+              <img src={PlusIcon} alt="icon-plus" />
+            </MenuButton>
             <MenuList className="app-query-menu__list">
               <MenuItem onClick={onOpenModalAddVisualization}>
                 <Flex alignItems={'center'} gap={'8px'}>
