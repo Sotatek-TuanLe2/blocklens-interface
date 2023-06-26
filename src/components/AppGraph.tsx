d80fa5dacf9dbb5bee2ba7d93d795bf775ebdd1b
Sotatek-ThangTran3 <thang.tran3@sotatek.com>
2023-06-26 11:30:16 +0700

    feat: update ui user graph

--- src/components/AppGraph.tsx
+++ src/components/AppGraph.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex } from '@chakra-ui/react';
-import React, { FC, useMemo, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import {
   LineChart,
   Line,
@@ -12,15 +12,38 @@ import {
 import { isMobile } from 'react-device-detect';
 import { formatTimestamp } from 'src/utils/utils-helper';
 import { formatNumber } from 'src/utils/utils-format';
-import { RadioChecked, RadioNoCheckedIcon } from 'src/assets/icons';
 
 interface IChart {
   data: any[];
   duration: string;
 }
 
+const CustomTooltip = (props: any) => {
+  const { active, payload, label } = props;
+  if (active && payload && payload.length) {
+    return (
+      <Box p={2}>
+        {`${label}`}
+
+        <Box>{`${payload[1].name} : ${formatNumber(
+          payload[1].value,
+          4,
+          '0',
+        )}`}</Box>
+        <Box>{`${payload[0].name} : ${formatNumber(
+          payload[0].value,
+          4,
+          '0',
+        )}`}</Box>
+      </Box>
+    );
+  }
+
+  return null;
+};
+
 const AppGraph: FC<IChart> = ({ data, duration }) => {
-  const [lineHide, setLineHide] = useState<string>('activities');
+  const [key, setKey] = useState(0);
   const dataChart = useMemo(() => {
     if (duration === '24h') {
       return data.map((item: any) => {
@@ -39,23 +62,17 @@ const AppGraph: FC<IChart> = ({ data, duration }) => {
     });
   }, [duration, data]);
 
-  const CustomTooltip = ({ active, payload, label }: any) => {
-    if (active && payload && payload.length) {
-      return (
-        <Box p={2}>{`${label} : ${formatNumber(
-          payload[0].value,
-          4,
-          '0',
-        )}`}</Box>
-      );
-    }
-
-    return null;
-  };
+  useEffect(() => {
+    setKey((pre) => pre + 1);
+  }, [data]);
 
   return (
     <Box height={isMobile ? '400px' : '500px'} px={isMobile ? 0 : 5}>
-      <ResponsiveContainer width="100%" height={isMobile ? '75%' : '85%'}>
+      <ResponsiveContainer
+        key={key}
+        width="100%"
+        height={isMobile ? '75%' : '85%'}
+      >
         <LineChart
           width={500}
           height={300}
@@ -67,69 +84,59 @@ const AppGraph: FC<IChart> = ({ data, duration }) => {
             bottom: 5,
           }}
         >
+          <CartesianGrid vertical={false} horizontal stroke="#E8EAED" />
+
+          <Line
+            key={key}
+            name="Numbers of messages"
+            dataKey="message"
+            stroke="#3A95FF"
+            strokeWidth={2}
+            dot={{
+              r: isMobile ? 2 : 8,
+              stroke: 'white',
+              strokeWidth: 3,
+              fill: '#3A95FF',
+            }}
+          />
+
+          <Line
+            key={key + 1}
+            dot={{
+              r: isMobile ? 2 : 8,
+              stroke: 'white',
+              strokeWidth: 3,
+              fill: '#FFB547',
+            }}
+            strokeWidth={2}
+            dataKey="activities"
+            stroke="#FFB547"
+            name="Numbers of activities"
+          />
           <XAxis
             dataKey="label"
             interval={isMobile ? undefined : 3}
-            tick={{ fill: 'rgba(0, 2, 36, 0.50)' }}
             axisLine={false}
+            dy={10}
+            tickLine={false}
+            domain={['auto', 'auto']}
+            padding={{ left: 20, right: 20 }}
           />
           <YAxis
-            tick={{ fill: 'rgba(0, 2, 36, 0.50)' }}
+            tickLine={false}
             tickFormatter={(value: any) => formatNumber(value, 4, '0')}
             axisLine={false}
           />
           <Tooltip content={<CustomTooltip />} />
-          {lineHide === 'message' && (
-            <Line
-              name="Numbers of messages"
-              dataKey="message"
-              stroke="#3A95FF"
-              strokeWidth={2}
-              dot={{ r: isMobile ? 2 : 4 }}
-            />
-          )}
-
-          {lineHide === 'activities' && (
-            <Line
-              dot={{ r: isMobile ? 2 : 4 }}
-              strokeWidth={2}
-              dataKey="activities"
-              stroke="#FFB547"
-              name="Numbers of activities"
-            />
-          )}
-          <CartesianGrid vertical={false} horizontal stroke="#E8EAED" />
         </LineChart>
       </ResponsiveContainer>
 
       <Flex my={isMobile ? 3 : 5} className={'legend'}>
-        <Flex
-          onClick={() => {
-            if (lineHide === 'message') {
-              setLineHide('activities');
-              return;
-            }
-            setLineHide('message');
-          }}
-        >
-          {lineHide === 'activities' ? (
-            <RadioChecked />
-          ) : (
-            <RadioNoCheckedIcon />
-          )}
+        <Flex>
           <Box className={`activities`}>Numbers of activities</Box>
         </Flex>
 
-        <Flex
-          onClick={() => {
-            if (lineHide === 'activities') {
-              setLineHide('message');
-              return;
-            }
-            setLineHide('activities');
-          }}
-        >
-          {lineHide === 'message' ? <RadioChecked /> : <RadioNoCheckedIcon />}
+        <Flex>
           <Box className={`message`}>Numbers of messages</Box>
         </Flex>
       </Flex>
