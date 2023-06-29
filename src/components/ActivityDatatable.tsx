a210f3f12964916f0a6e2fd13b0a5170829cdb05
Minh Chau <minhchau.dr27@gmail.com>
2023-06-29 12:00:52 +0700

    fix: fix search by tag

--- src/components/ActivityDatatable.tsx
+++ src/components/ActivityDatatable.tsx
@@ -55,7 +55,8 @@ const _renderStatus = (activity: IActivityResponse) => {
 
   if (activity?.lastStatus === STATUS.PROCESSING) {
     return (
-      <Box className="status waiting">Processing
+      <Box className="status waiting">
+        Processing
         {/*{activity?.retryTime}/5*/}
       </Box>
     );
