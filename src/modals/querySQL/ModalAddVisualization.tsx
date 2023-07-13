e894c00bd76de5ec210d04bf4b5a17dff25e18c7
Sotatek-Tienpham <tien.pham@sotatek.com>
2023-07-13 09:42:55 +0700

    fix: refactor code

--- src/modals/querySQL/ModalAddVisualization.tsx
+++ src/modals/querySQL/ModalAddVisualization.tsx
@@ -86,7 +86,7 @@ const ModalAddVisualization: React.FC<IModalAddVisualization> = ({
     [],
   );
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [widthWidget, setWidthWidget] = useState<number>(TOTAL_COL / 2);
+  const [widthWidget, setWidthWidget] = useState<number>(0);
   const [dataVisualPagination, setDataVisualPagination] = useState<
     IPagination | undefined
   >();
@@ -162,7 +162,7 @@ const ModalAddVisualization: React.FC<IModalAddVisualization> = ({
       setWidthWidget(TOTAL_COL);
     } else if (visualSelected?.type === TYPE_VISUALIZATION.counter) {
       setWidthWidget(TOTAL_COL / 4);
-    } else {
+    } else if (visualSelected?.type) {
       setWidthWidget(TOTAL_COL / 2);
     }
   }, [visualSelected]);
@@ -342,14 +342,14 @@ const ModalAddVisualization: React.FC<IModalAddVisualization> = ({
               return (
                 <Flex
                   mr={{ base: 2, md: 3 }}
-                  onClick={() =>
-                    !visualSelected ? null : setWidthWidget(item.col)
+                  onClick={
+                    !visualSelected ? undefined : () => setWidthWidget(item.col)
                   }
-                  cursor={'pointer'}
+                  cursor={!visualSelected ? 'default' : 'pointer'}
                   key={index}
                   mb={{ base: 3, md: 0 }}
                 >
-                  {widthWidget === item.col && !!visualSelected ? (
+                  {widthWidget === item.col ? (
                     <RadioChecked />
                   ) : (
                     <RadioNoCheckedIcon />
@@ -409,11 +409,7 @@ const AddVisualizationCheckbox: React.FC<IAddVisualizationCheckbox> = ({
   };
 
   const handleCheckboxChange = (data: IListMyQueries) => {
-    if (visualSelected?.id === data.id) {
-      setVisualSelected('');
-    } else {
-      setVisualSelected(data);
-    }
+    setVisualSelected(data);
   };
 
   return (
