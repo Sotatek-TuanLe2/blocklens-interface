6de528eedf7d2d757e837edd729e83616bc161f0
tuan.le2 <tuan.le2@sotatek.com>
2023-08-07 14:56:28 +0700

    fix: [Create Token activity - Aptos webhook] Ko trim space khi user input [Name] có space đầu/cuối

--- src/pages/CreateWebhookPage/index.tsx
+++ src/pages/CreateWebhookPage/index.tsx
@@ -160,7 +160,7 @@ const CreateWebhook = () => {
             ?.split(',')
             .filter((item: string) => !!item)
             .map((item: string) => +item.trim()) || [],
-        name: dataForm?.metadata?.name,
+        name: dataForm?.metadata?.name ? dataForm?.metadata?.name.trim() : '',
       },
     };
 
