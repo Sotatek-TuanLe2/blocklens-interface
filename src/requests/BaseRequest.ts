9f73e52842b6020ea609fc1e664cbfeac6bca997
tuan.le2 <tuan.le2@sotatek.com>
2023-08-28 14:40:16 +0700

    feat: apply refreshToken

--- src/requests/BaseRequest.ts
+++ src/requests/BaseRequest.ts
@@ -10,6 +10,69 @@ import {
 import { AppBroadcast } from 'src/utils/utils-broadcast';
 import { RECAPTCHA_ACTIONS } from 'src/utils/common';
 import { COMMON_ERROR_MESSAGE } from 'src/constants';
+import store from 'src/store';
+import { setUserAuth } from 'src/store/user';
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async (error) => {
+    const { response, config } = error;
+    const status = response?.status;
+
+    if (
+      status === 401 &&
+      response.data?.message.toString() !== 'Credential is not correct'
+    ) {
+      const refreshToken = Storage.getRefreshToken();
+      if (!refreshToken) {
+        return AppBroadcast.dispatch('REQUEST_SIGN_IN');
+      }
+
+      try {
+        const response = await axios.post(
+          config.api.baseUrlApi + '/public/users/refresh-token',
+          { refreshToken },
+        );
+        store().store.dispatch(setUserAuth(response));
+        return axios(error.config);
+      } catch (error) {
+        return AppBroadcast.dispatch('REQUEST_SIGN_IN');
+      }
+    }
+
+    if (status === 403) {
+      return AppBroadcast.dispatch('LOGOUT_USER');
+    }
+
+    if (response) {
+      console.log('===errorHandler', JSON.stringify(response));
+      console.log('===errorHandler data', JSON.stringify(response.data));
+      console.log('===errorHandler status', JSON.stringify(status));
+      console.log('===errorHandler headers', JSON.stringify(response.headers));
+    } else {
+      console.log('==errorHandler', JSON.stringify(error));
+    }
+
+    if (status === 0 || status === 500) {
+      // Network error
+      throw new Error(COMMON_ERROR_MESSAGE);
+    }
+
+    if (response && response.data && response.data.message) {
+      if (typeof response.data.message === 'string') {
+        throw new Error(response.data.message);
+      }
+      throw new Error(response.data.message[0]);
+    }
+    if (response && response.data && response.data.error) {
+      throw new Error(response.data.error);
+    }
+
+    throw error;
+  },
+);
 
 export default class BaseRequest {
   protected accessToken = '';
@@ -43,33 +106,21 @@ export default class BaseRequest {
   }
 
   async get(url: string, params?: any) {
-    try {
-      const config = {
-        params,
-      };
-      const response = await axios.get(this.getUrlPrefix() + url, config);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
-    }
+    const config = {
+      params,
+    };
+    const response = await axios.get(this.getUrlPrefix() + url, config);
+    return this._responseHandler(response);
   }
 
   async put(url: any, data?: any) {
-    try {
-      const response = await axios.put(this.getUrlPrefix() + url, data);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
-    }
+    const response = await axios.put(this.getUrlPrefix() + url, data);
+    return this._responseHandler(response);
   }
 
   async patch(url: any, data?: any) {
-    try {
-      const response = await axios.patch(this.getUrlPrefix() + url, data);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
-    }
+    const response = await axios.patch(this.getUrlPrefix() + url, data);
+    return this._responseHandler(response);
   }
 
   async post(
@@ -77,95 +128,31 @@ export default class BaseRequest {
     data?: any,
     recaptchaAction?: typeof RECAPTCHA_ACTIONS[keyof typeof RECAPTCHA_ACTIONS],
   ) {
-    try {
-      if (recaptchaAction) {
-        await this.getAndSetRecaptcha(recaptchaAction);
-      }
-      const response = await axios.post(this.getUrlPrefix() + url, data);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
+    if (recaptchaAction) {
+      await this.getAndSetRecaptcha(recaptchaAction);
     }
+    const response = await axios.post(this.getUrlPrefix() + url, data);
+    return this._responseHandler(response);
   }
 
   async delete(url: any, data?: any) {
-    try {
-      const config = {
-        data,
-      };
-      const response = await axios.delete(this.getUrlPrefix() + url, config);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
-    }
+    const config = {
+      data,
+    };
+    const response = await axios.delete(this.getUrlPrefix() + url, config);
+    return this._responseHandler(response);
   }
 
   async download(url: any, data?: any) {
-    try {
-      const config = {
-        ...data,
-        responseType: 'blob',
-      };
-      const response = await axios.get(this.getUrlPrefix() + url, config);
-      return this._responseHandler(response);
-    } catch (error) {
-      return this._errorHandler(error);
-    }
+    const config = {
+      ...data,
+      responseType: 'blob',
+    };
+    const response = await axios.get(this.getUrlPrefix() + url, config);
+    return this._responseHandler(response);
   }
 
   async _responseHandler(response: any) {
     return response.data;
-  }
-
-  _error403Handler() {
-    return AppBroadcast.dispatch('LOGOUT_USER');
-  }
-
-  _error401Handler() {
-    return AppBroadcast.dispatch('REQUEST_SIGN_IN');
-  }
-
-  async _errorHandler(err: any) {
-    if (
-      err.response?.status === 401 &&
-      err.response.data?.message.toString() !== 'Credential is not correct'
-    ) {
-      return this._error401Handler();
-    }
-
-    if (err.response?.status === 403) {
-      return this._error403Handler();
-    }
-
-    if (err.response) {
-      console.log('===errorHandler', JSON.stringify(err.response));
-      console.log('===errorHandler data', JSON.stringify(err.response.data));
-      console.log(
-        '===errorHandler status',
-        JSON.stringify(err.response?.status),
-      );
-      console.log(
-        '===errorHandler headers',
-        JSON.stringify(err.response.headers),
-      );
-    } else {
-      console.log('==errorHandler', JSON.stringify(err));
-    }
-
-    if (err.response?.status === 0 || err.response?.status === 500) {
-      // Network error
-      throw new Error(COMMON_ERROR_MESSAGE);
-    }
-
-    if (err.response && err.response.data && err.response.data.message) {
-      if (typeof err.response.data.message === 'string') {
-        throw new Error(err.response.data.message);
-      }
-      throw new Error(err.response.data.message[0]);
-    }
-    if (err.response && err.response.data && err.response.data.error) {
-      throw new Error(err.response.data.error);
-    }
-    throw err;
   }
 }
