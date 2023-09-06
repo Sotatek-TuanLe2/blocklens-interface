a9108e29590a6e0c0e5e67b0e7a63345894fc261
DinhTran <dinh.tran2@sotatek.com>
2023-09-06 10:26:00 +0700

    fix bls 1267

--- src/pages/WebHookCreatePage/parts/PartFormAddressActivity.tsx
+++ src/pages/WebHookCreatePage/parts/PartFormAddressActivity.tsx
@@ -144,9 +144,14 @@ const PartFormAddressActivity: FC<IPartFormAddressActivity> = ({
     reader.onload = (e: any) => {
       const data = e.target.result;
       const dataFormat = data?.split('\n').filter((item: string) => !!item);
-      setAddressesValue(
-        dataFormat.map((item: string) => item.replace('\r', '')).join('\n'),
-      );
+      const uploadedAddresses = dataFormat
+        .map((item: string) => item.replace('\r', ''))
+        .join('\n');
+      if (!uploadedAddresses) {
+        toastError({ message: 'The Addresses file must be correct format' });
+        return;
+      }
+      setAddressesValue(uploadedAddresses);
       setFileSelected(dropFile || evt.target.files[0]);
     };
 
