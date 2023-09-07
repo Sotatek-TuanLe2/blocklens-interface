e1406b3d6850a15db62862542483c88f43c1f4bb
DinhTran <dinh.tran2@sotatek.com>
2023-09-07 14:24:14 +0700

    selected for item app menu

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -10,6 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { IDataMenu } from '../utils/utils-app';
+import 'src/styles/components/AppMenu.scss';
 
 interface IAppMenuProps {
   data: IDataMenu[];
@@ -28,74 +29,82 @@ const AppMenu: FC<IAppMenuProps> = ({ data, value, setValue, minW }) => {
   }, [value]);
 
   return (
-    <Menu>
-      <MenuButton w={'full'} px={0} py={0}>
-        {!!currentData && (
-          <Flex
-            align={'center'}
-            justify={'center'}
-            h={'40px'}
-            minW={minW}
-            bg={'white'}
-            borderRadius={'6px'}
-            border={'1px solid #C7D2E1'}
-            px={'20px'}
-            py={'10px'}
-          >
-            <Flex align={'center'}>
-              {currentData.icon && (
-                <Box mr={2} color={'rgba(0, 2, 36, 0.5)'}>
-                  {currentData.icon}
-                </Box>
-              )}
-              <Text className={'text-filter'}>{currentData.label}</Text>
-            </Flex>
-            <Box ml={2}>
-              <ChevronDownIcon />
-            </Box>
-          </Flex>
-        )}
-      </MenuButton>
-
-      <MenuList minW={'124px'} mt={-2} zIndex={4}>
-        {data.map((item, index) => (
-          <MenuItem
-            key={index}
-            px={0}
-            py={0}
-            onClick={() => {
-              setCurrentData(item);
-              setValue(item.value);
-            }}
-          >
+    <div className="app-menu">
+      <Menu>
+        <MenuButton w={'full'} px={0} py={0}>
+          {!!currentData && (
             <Flex
               align={'center'}
+              justify={'center'}
               h={'40px'}
               minW={minW}
+              bg={'white'}
+              borderRadius={'6px'}
+              border={'1px solid #C7D2E1'}
               px={'20px'}
               py={'10px'}
-              sx={{
-                _hover: {
-                  bg: '#0060DB',
-                  '& *': {
-                    color: 'white !important',
-                  },
-                },
-              }}
             >
               <Flex align={'center'}>
-                {item.icon && (
+                {currentData.icon && (
                   <Box mr={2} color={'rgba(0, 2, 36, 0.5)'}>
-                    {item.icon}
+                    {currentData.icon}
                   </Box>
                 )}
-                <Text>{item.label}</Text>
+                <Text className={'text-filter'}>{currentData.label}</Text>
               </Flex>
+              <Box ml={2}>
+                <ChevronDownIcon />
+              </Box>
             </Flex>
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
+          )}
+        </MenuButton>
+
+        <MenuList
+          minW={'124px'}
+          mt={-2}
+          zIndex={4}
+          className="app-menu__menu-item"
+        >
+          {data.map((item, index) => (
+            <MenuItem
+              className={`${value === item?.value ? 'selected' : ''}`}
+              key={index}
+              px={0}
+              py={0}
+              onClick={() => {
+                setCurrentData(item);
+                setValue(item.value);
+              }}
+            >
+              <Flex
+                align={'center'}
+                h={'40px'}
+                minW={minW}
+                px={'20px'}
+                py={'10px'}
+                sx={{
+                  _hover: {
+                    bg: '#0060DB',
+                    '& *': {
+                      color: 'white !important',
+                    },
+                  },
+                }}
+              >
+                <Flex align={'center'}>
+                  {item.icon && (
+                    <Box mr={2} color={'rgba(0, 2, 36, 0.5)'}>
+                      {item.icon}
+                    </Box>
+                  )}
+                  <Text>{item.label}</Text>
+                </Flex>
+              </Flex>
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </div>
   );
 };
 
