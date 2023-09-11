b0abd1aa43818e615a8df093e4aee43e2ee7bcd2
tuan.le2 <tuan.le2@sotatek.com>
2023-09-11 11:32:31 +0700

    fix: refactor AppPagination for deployment

--- src/components/AppPagination.tsx
+++ src/components/AppPagination.tsx
@@ -4,34 +4,30 @@ import 'src/styles/components/AppPagination.scss';
 import { ArrowDownIcon } from '../assets/icons';
 import { isMobile } from 'react-device-detect';
 
-const AppPagination: FC<ReactPaginateProps> = ({ ...props }) => {
-  return (
-    <ReactPaginate
-      breakLabel="..."
-      nextLabel={
-        <ArrowDownIcon
-          style={{
-            transform: 'rotate(270deg)',
-            cursor:
-              props.forcePage === props.pageCount - 1
-                ? 'not-allowed'
-                : 'pointer',
-          }}
-        />
-      }
-      pageRangeDisplayed={isMobile ? 1 : 2}
-      marginPagesDisplayed={isMobile ? 1 : 2}
-      previousLabel={
-        <ArrowDownIcon
-          style={{
-            transform: 'rotate(90deg)',
-            cursor: props.forcePage === 0 ? 'not-allowed' : 'pointer',
-          }}
-        />
-      }
-      className="app-pagination"
-      {...props}
-    />
-  );
-};
+const AppPagination: FC<ReactPaginateProps> = (props) => (
+  <ReactPaginate
+    breakLabel="..."
+    nextLabel={
+      <ArrowDownIcon
+        style={{
+          transform: 'rotate(270deg)',
+          cursor:
+            props.forcePage === props.pageCount - 1 ? 'not-allowed' : 'pointer',
+        }}
+      />
+    }
+    pageRangeDisplayed={isMobile ? 1 : 2}
+    marginPagesDisplayed={isMobile ? 1 : 2}
+    previousLabel={
+      <ArrowDownIcon
+        style={{
+          transform: 'rotate(90deg)',
+          cursor: props.forcePage === 0 ? 'not-allowed' : 'pointer',
+        }}
+      />
+    }
+    className="app-pagination"
+    {...props}
+  />
+);
 export default AppPagination;
