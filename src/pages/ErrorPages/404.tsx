f17c6bdea307cd30fedcc06cbd329b888126fd84
tuan.le2 <tuan.le2@sotatek.com>
2023-09-19 14:06:10 +0700

    feat: finish 404 page

--- src/pages/ErrorPages/404.tsx
+++ src/pages/ErrorPages/404.tsx
@@ -1,3 +1,36 @@
-const Error404Page = () => <>404</>;
+import { Flex, Heading, Stack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { AppButton } from 'src/components';
+import { BasePage } from 'src/layouts';
+import 'src/styles/pages/ErrorPages.scss';
+import { ROUTES } from 'src/utils/common';
+
+const Error404Page = () => (
+  <BasePage isFullWidth>
+    <Flex
+      justifyContent={'center'}
+      alignItems={'center'}
+      className="error-page"
+    >
+      <Stack textAlign={'center'} spacing={'20px'} alignItems="center">
+        <img
+          src="/images/img-not-found.png"
+          alt="404 not found"
+          width={'50%'}
+        />
+        <Heading size={'md'}>
+          {
+            'The page you are looking for might be removed\n or is temporarily unavailable'
+          }
+        </Heading>
+        <Link to={ROUTES.HOME}>
+          <AppButton width={'fit-content'} borderRadius={20}>
+            Go back home
+          </AppButton>
+        </Link>
+      </Stack>
+    </Flex>
+  </BasePage>
+);
 
 export default Error404Page;
